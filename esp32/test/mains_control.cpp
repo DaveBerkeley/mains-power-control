8c40b9008e7d28d23272b502c34acac7913e0bdc
Dave Berkeley <dave@rotwang.co.uk>
2026-01-25 17:14:14 +0000

    Added unit tests for TemperatureControl

--- esp32/test/mains_control.cpp
+++ esp32/test/mains_control.cpp
@@ -15,159 +15,15 @@
 #include "panglos/app/event.h"
 
 #include "mains_control.h"
+#include "devices.h"
 
 using namespace panglos;
-
-    /*
-     *
-     */
-
-class mc_GPIO : public panglos::GPIO
-{
-public:
-    bool state;
-
-    mc_GPIO() : state(true) { }
-
-    virtual void set(bool on) override
-    {
-        state = on;
-        PO_DEBUG("state=%d", state);
-    }
-    virtual bool get() override
-    {
-        return state;
-    }
-    virtual void toggle() override
-    {
-        set(!get());
-    }
-};
-
-class mc_LedStrip : public LedStrip
-{
-public:
-    struct LedState {
-        uint8_t r;
-        uint8_t g;
-        uint8_t b;
-    };
-
-    struct LedState state[2];
-
-    mc_LedStrip()
-    {
-        memset(state, 0, sizeof(state));
-    }
-
-    virtual void set(int led, uint8_t r, uint8_t g, uint8_t b) override
-    {
-        //PO_DEBUG("idx=%d r=%d g=%d b=%d", led, r, g, b);
-        EXPECT_TRUE(led >= 0);
-        EXPECT_TRUE(led < num_leds());
-        state[led].r = g; // r-g swapped on YF923 devices
-        state[led].g = r;
-        state[led].b = b;
-    }
-    virtual void set_all(uint8_t r, uint8_t g, uint8_t b) override
-    {
-        PO_DEBUG("r=%d g=%d b=%d", r, g, b);
-        for (int i = 0; i < num_leds(); i++)
-        {
-            state[i].r = g; // r-g swapped on YF923 devices
-            state[i].g = r;
-            state[i].b = b;
-        }
-    }
-    virtual bool send() override
-    {
-        //PO_DEBUG("");
-        return true;
-    }
-    virtual int num_leds() override
-    {
-        //PO_DEBUG("");
-        return 2;
-    }    
-};
-
-class mc_UART : public panglos::UART
-{
-public:
-    char buff[64];
-    size_t idx;
-    typedef std::list<std::string> List;
-
-    List strings;
-
-    mc_UART() : idx(0) { }
-
-    virtual int rx(char* data, int n) override
-    {
-        PO_DEBUG("%p %d", data, n);
-        return n;
-    }
-    virtual int tx(const char* data, int n) override
-    {
-        //PO_DEBUG("%p '%c' %d", data, *data, n);
-        for (int i = 0; i < n; i++)
-        {
-            ASSERT(idx < sizeof(buff));
-            switch (data[i])
-            {
-                case '\r' : continue;
-                case '\n':
-                {
-                    buff[idx] = '\0';
-                    strings.push_back(buff);
-                    //PO_DEBUG("'%s'", buff);
-                    idx = 0;
-                    continue;
-                }
-                default : break;
-            }
-            buff[idx++] = data[i];
-        }
-        return n;
-    }
-
-    bool pop_string(char *buff, size_t len)
-    {
-        if (strings.size() == 0) return false;
-        std::string s = strings.front();
-        strncpy(buff, s.c_str(), len);
-        strings.pop_front();
-        return true;
-    }
-};
-
-class mc_Temp : public panglos::TemperatureSensor
-{
-public:
-    double t;
-
-    virtual bool get_temp(double *pt) override
-    {
-        if (pt) *pt = t;
-        return true;
-    }
-    virtual bool ready() override
-    {
-        return true;
-    }
-    virtual bool start_conversion() override
-    {
-        return true;
-    }
-
-    mc_Temp() : t(0) { }
-};
 
      /*
      *  Simulate a system populated with Devices
      */
 
-class SysSetup
+class mc_SysSetup
 {
 public:
     mc_GPIO button;
@@ -176,7 +32,7 @@ public:
     mc_UART uart;
     mc_Temp temperature;
 
-    SysSetup()
+    mc_SysSetup()
     {
         // make sure it looks a bit like a recent boot
         Time::set(1);
@@ -191,7 +47,7 @@ public:
         Objects::objects->add("leds", & leds);
         Objects::objects->add("uart", & uart);
     }
-    ~SysSetup()
+    ~mc_SysSetup()
     {
         delete Objects::objects;
         Objects::objects = 0;
@@ -204,7 +60,7 @@ public:
 
 TEST(MainsControl, Modes)
 {
-    SysSetup sys;
+    mc_SysSetup sys;
     PowerControl *control = PowerControl::create(1000, -30);
     TemperatureControlConfig tc_config = {
         .fan = 0,
@@ -270,7 +126,7 @@ TEST(MainsControl, Modes)
 
 TEST(MainsControl, Keys)
 {
-    SysSetup sys;
+    mc_SysSetup sys;
     PowerControl *control = PowerControl::create(1000, -30);
     TemperatureControlConfig tc_config = {
         .fan = 0,
@@ -340,7 +196,7 @@ TEST(MainsControl, Keys)
 
 TEST(MainsControl, Power)
 {
-    SysSetup sys;
+    mc_SysSetup sys;
     PowerControl *control = PowerControl::create(1000, -30);
     TemperatureControlConfig tc_config = {
         .fan = 0,
@@ -560,7 +416,7 @@ TEST(MainsControl, Solar)
 
 TEST(MainsControl, Fan)
 {
-    SysSetup sys;
+    mc_SysSetup sys;
     PowerControl *control = PowerControl::create(1000, -30);
     TemperatureControlConfig tc_config = {
         .fan = & sys.fan,
---
+++ esp32/test/temperature.cpp
@@ -0,0 +1,125 @@
+
+#include <gtest/gtest.h>
+
+#include "panglos/debug.h"
+#include "panglos/debug.h"
+
+#include "temperature.h"
+
+// dummy devices
+#include "devices.h"
+
+using namespace panglos;
+
+class t_SysSetup
+{
+public:
+    mc_GPIO fan;
+    mc_Temp temperature;
+
+    t_SysSetup()
+    {
+        // make sure it looks a bit like a recent boot
+        Time::set(1);
+    }
+    ~t_SysSetup()
+    {
+    }
+};
+
+    /*
+     *
+     */
+
+TEST(Temperature, Update)
+{
+    t_SysSetup sys;
+    TemperatureControlConfig config = {
+        .fan = & sys.fan,
+        .sensor = & sys.temperature,
+        .fan_on = 28,
+        .fan_off = 25,
+        .alarm = 40,
+    };
+
+    TemperatureControl tc(& config);
+
+    for (int t = -20; t < 80; t++)
+    {
+        sys.temperature.t = t;
+        tc.update();
+        EXPECT_EQ(tc.get_temperature(), t);
+    }
+}
+
+TEST(Temperature, Alarm)
+{
+    t_SysSetup sys;
+    TemperatureControlConfig config = {
+        .fan = & sys.fan,
+        .sensor = & sys.temperature,
+        .fan_on = 28,
+        .fan_off = 25,
+        .alarm = 40,
+    };
+
+    TemperatureControl tc(& config);
+
+    for (int t = -20; t < 80; t++)
+    {
+        sys.temperature.t = t;
+        tc.update();
+        if (t < 40)
+        {
+            EXPECT_FALSE(tc.alarm());
+        }
+        else
+        {
+            EXPECT_TRUE(tc.alarm());
+        }
+    }
+}
+
+TEST(Temperature, Fan)
+{
+    t_SysSetup sys;
+    TemperatureControlConfig config = {
+        .fan = & sys.fan,
+        .sensor = & sys.temperature,
+        .fan_on = 28,
+        .fan_off = 25,
+        .alarm = 40,
+    };
+
+    TemperatureControl tc(& config);
+
+    for (int t = -20; t < 80; t++)
+    {
+        sys.temperature.t = t;
+        tc.update();
+        if (t <= 28)
+        {
+            EXPECT_FALSE(sys.fan.state);
+        }
+        else
+        {
+            EXPECT_TRUE(sys.fan.state);
+        }
+    }
+
+    for (int t = 80; t > -20; t--)
+    {
+        sys.temperature.t = t;
+        tc.update();
+        if (t >= 25)
+        {
+            EXPECT_TRUE(sys.fan.state);
+        }
+        else
+        {
+            EXPECT_FALSE(sys.fan.state);
+        }
+    }
+}
+
+//  FIN
